9e200450ed123b8d35525c3fac11830f3920ffb2
Andre P <85500266+andrepaca@users.noreply.github.com>
2021-09-05 02:05:28 +0300

    update event info

--- src/constants/events.ts
+++ src/constants/events.ts
@@ -25,23 +25,21 @@ export const EVENTS_INFO: {
   [ChainId.BSC_MAINNET]: [
     {
       title: 'BTC Price Guess OLD',
-      desc:
-        'You win if you correctly guess the price of BTC. Will it be higher or lower then $52,400 USD at 11 September 23:59 UTC? Witdraw from this pool',
+      desc: 'WITHDRAW FROM THIS POOL, and deposit to the new POOL',
       address: '0xaF7BF584be79d5335ac1c2404215B3F809684bA2',
       tokens: getPairTokensWithDefaults(ChainId.BSC_MAINNET, 'WBNB/DUEL'),
       active: true,
       startBlock: 10630150,
-      endBlock: 10836621,
+      endBlock: 10630150,
       pools: [
         { pid: 0, title: 'BTC HIGHER', img1: '/images/events/btc.png', img2: '/images/events/duel_dark.png' },
         { pid: 1, title: 'BTC LOWER', img1: '/images/events/btc.png', img2: '/images/events/duel_dark.png' }
       ]
-    }
-  ],
- {
+    },
+    {
       title: 'BTC Price Guess NEW',
       desc:
-        'You win if you correctly guess the price of BTC. Will it be higher or lower then $52,400 USD at 11 September 23:59 UTC? Deposit the NEW pool',
+        'You win if you correctly guess the price of BTC. Will it be higher or lower then $52,400 USD at 11 September 23:59 UTC?',
       address: '0xC231470e926678CfE8539acdd8b748082A2e4476',
       tokens: getPairTokensWithDefaults(ChainId.BSC_MAINNET, 'WBNB/DUEL'),
       active: true,
