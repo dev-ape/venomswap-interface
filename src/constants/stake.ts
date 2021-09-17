fd1bfd3906431dec4c93ffccf0635bedaaf100fa
Andre P <85500266+andrepaca@users.noreply.github.com>
2021-09-17 17:11:19 +0300

    add stake config

--- src/constants/stake.ts
+++ src/constants/stake.ts
@@ -9,7 +9,10 @@ export interface StakeInfo {
 export const STAKE_INFO: {
   [chainId in ChainId]?: StakeInfo | undefined
 } = {
-  [ChainId.BSC_MAINNET]: undefined,
+  [ChainId.BSC_MAINNET]: {
+    address: '0x367cdB240393b341600764f1a2F36650D2f7296a',
+    tokens: getPairTokensWithDefaults(ChainId.BSC_MAINNET, 'DUEL/DUEL')
+  },
   [ChainId.BSC_TESTNET]: {
     address: '0x917bA6835537EF1faf7c5B586063A6b4BBE0E652',
     tokens: getPairTokensWithDefaults(ChainId.BSC_TESTNET, 'DUEL/DUEL')
